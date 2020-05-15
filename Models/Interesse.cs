ea95918e5e92fd0d06d862aa6665c96c12efa977
Davidson Eustaquio <davideustaquio@gmail.com>
2020-05-14 22:15:23 -0300

    Classe Interesse
    Criação da classe Interesse

---
+++ Models/Interesse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaoLendario.Models
+{
+    public class Interesse : Adotante
+    {
+        private DateTime data;
+        private Boolean adotado;
+
+        private DateTime Data { get; set; }
+        private Boolean Ddotado { get; set; }
+    }
+}
